72254eb49109b68eb2d52a0d1e752ef0cafab1da
Anastasiia <nastya.zkhrvvvv@gmail.com>
2024-07-13 18:59:03 +0200

    comments, formatting

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -7,6 +7,7 @@ const { RegisterPage } = require('../page-objects/register-page')
 test('has title', async ({ page }) => {
   const homePage = new HomePage(page);
   await homePage.goto();
+
   // Expect a title to contain "Redmine".
   await expect(page).toHaveTitle(/Redmine/);
 });
@@ -14,10 +15,9 @@ test('has title', async ({ page }) => {
 test('help link', async ({ page }) => {
   const homePage = new HomePage(page);
   await homePage.goto();
+
   // Click the help link.
   await homePage.clickOnHelpLink();
-
-  // await page.getByRole('link', { name: 'Help',exact: true }).click();
 
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
@@ -26,10 +26,13 @@ test('help link', async ({ page }) => {
 test('search by id', async ({ page }) => {
   const homePage = new HomePage(page);
   await homePage.goto();
+  
+  // Generate random number
   let randomId = createRandomNumber(4);
 
   // Enter the text in search field.
   await homePage.performSearch(randomId);
+
   // Expects page to have a heading with the name of Feature #40970.
   await expect(page.getByRole('heading', { name: 'Feature #' + randomId })).toBeVisible();
 });
@@ -37,10 +40,13 @@ test('search by id', async ({ page }) => {
 test('forum boards', async ({ page }) => {
   const homePage = new HomePage(page);
   await homePage.goto();
+
   // Click the forum link.
   await homePage.clickOnForumLink();
+
   // Expects page to have a heading with the name of Forums.
   await expect(page.getByRole('heading', { name: 'Forums' })).toBeVisible();
+
   // Expects page to have forum boards.
   const forumPage = new ForumPage(page);
   await expect(forumPage.openDiscussionLink).toBeVisible();
@@ -53,18 +59,22 @@ test('forum boards', async ({ page }) => {
 test('invalid email check on register page', async ({ page }) => {
   const homePage = new HomePage(page);
   await homePage.goto();
+
   // Click the register link.
   await homePage.clickOnRegisterLink();
+  
   // Expects page to have a heading with the name of Register.
   await expect(page.getByRole('heading', { name: 'Register' })).toBeVisible();
   const registerPage = new RegisterPage(page);
+
   // Enter the invalid email in email field.
   await registerPage.enterRandomInvalidEmail();
+
   // Click the submit button.
   await registerPage.clickOnSubmitButton();
+
   // Expects page to have an error message "Email is invalid".
   await expect(page.getByText('Email is invalid')).toBeVisible();
-
 });
 
 function createRandomNumber(length) {
